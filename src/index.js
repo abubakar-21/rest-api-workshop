7563d127cc7b0a5da2084764900a5f643806feab
Abubakar Abdulkader <abubakarabdulkader21@gmail.com>
2023-05-10 11:45:30 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const agareRoutes = require('../Routes/agare');
 const bilRoutes = require('../Routes/bilar');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const MONGODB_URL = process.env.MONGODB_URL;
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,7 +20,18 @@ app.use('/agare', agareRoutes);
 // Använd bilRoutes för alla bil-rutter
 app.use('/bilar', bilRoutes);
 
+mongoose
+    .connect(MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('MDB connected...'))
+    .catch((err) => console.log(err));
+
+app.use(express.json());
+
+
 // Starta servern på port 3000
-app.listen(3000, () => {
-    console.log('Servern lyssnar på port 3000');
+app.listen(3300, () => {
+    console.log('Servern lyssnar på port 3300');
 });
